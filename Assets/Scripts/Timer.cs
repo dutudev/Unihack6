0dce399f65b413b3ee70f4cf8a94246ef8e2e917
Razvan104 <razvanvitan73@gmail.com>
2024-11-08 18:12:24 +0200

    start

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+    public enum Time;
+    [SerializeField] TextMeshProGUI timerText; 
+
+   public void Buildtime()
+    {
+
+    }
+}
