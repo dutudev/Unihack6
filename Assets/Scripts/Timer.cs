aa248905a9c254ca39bfa605c87fee2c1459a28c
Razvan104 <razvanvitan73@gmail.com>
2024-11-08 21:21:54 +0200

    timerul

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -1,15 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 
 public class Timer : MonoBehaviour
 {
-    public enum Time;
-    [SerializeField] TextMeshProGUI timerText; 
+    [SerializeField] TextMeshProUGUI countdownText;
+    [SerializeField] float newFontSize = 75f;
 
-   public void Buildtime()
+    float currentTime = 0f;
+    float startingTime = 31f;
+
+
+   void Start()
     {
+        currentTime = startingTime;
+    }
 
+    void Update()
+    {
+        
+        if(currentTime < 0)
+        {
+            currentTime = 0;
+        }
+        else
+        {
+            countdownText.text = Mathf.FloorToInt(currentTime).ToString();
+            currentTime -= Time.deltaTime;
+        }
+        if(currentTime <= 6)
+        {
+            countdownText.color = Color.red;
+            countdownText.fontSize = newFontSize;
+        }
     }
 }
